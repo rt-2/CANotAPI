e1e166d5102048bfbbfe184c4755141d077bf139
Yanick <rt-2@rt-2.net>
2017-09-28 17:30:50 -0400

    Update example.php
    Updated example

--- example.php
+++ example.php
@@ -7,14 +7,21 @@
 			// Requires the CANotAPI script from this package
 			require('./CANotAPI.inc');
 			
-			// Show notams with a list or search words
+			//
+			//	Show notams with a list or search words
+			//
 			echo '<h2>Important Notams for CYUL</h2>';
-			//this function echos the html result
-			CANotAPI_EchoNotamsString('cyul', ['CLSD', 'rnw', 'twy', 'APCH']);
 			
-			// Show all notams by searching " " (a space).
+			// This function echos the html result
+			CANotAPI_EchoNotamsString('cyul', ['CLSD', 'APCH'], false);// only very important stuff
+			//CANotAPI_EchoNotamsString('cyul', ['CLSD', 'rnw', 'twy', 'APCH'], false); // 'extended' important stuff
+			
+			//
+			//	Show all notams by searching " " (a space).
+			//
 			echo '<h2>All Notams for CYUL</h2>';
-			//this function returns the html result
+			
+			// this function returns the html result
 			echo CANotAPI_GetNotamsString('cyul', ' ');
       
 		?>
